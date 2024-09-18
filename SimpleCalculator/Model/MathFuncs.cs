b0dd09b29c011e68570aec89b084b9155ff2fe9b
Ivan Zhenin <ofdof44488kkofsdkofs@mail.ru>
2024-09-18 21:16:27 +0500

    Add math-funcs class

---
+++ SimpleCalculator/Model/MathFuncs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Model
+{
+    public class MathFuncs
+    {
+        public static double Addition(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        public static double Subtract(double firstNum, double secondNum)
+        {
+            return firstNum - secondNum;
+        }
+
+        public static double Multiply(double firstNum, double secondNum)
+        {
+            return firstNum * secondNum;
+        }
+
+        public static double Divide(double firstNum, double secondNum)
+        {
+            return firstNum / secondNum;
+        }
+    }
+}
