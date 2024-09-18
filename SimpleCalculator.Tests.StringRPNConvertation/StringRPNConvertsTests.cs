6374a374202df52b6f088268990bbf0da076931d
Ivan Zhenin <ofdof44488kkofsdkofs@mail.ru>
2024-09-18 21:25:01 +0500

    Add RPN-Convertation tests

---
+++ SimpleCalculator.Tests.StringRPNConvertation/StringRPNConvertsTests.cs
@@ -0,0 +1,64 @@
+using SimpleCalculator.Model;
+
+namespace SimpleCalculator.Tests.StringRPNConvertation
+{
+    [TestClass]
+    public class StringRPNConvertsTests
+    {
+        /// <summary>
+        /// Тест конвертации строки "2+2*2" в "2 2 2 * +"
+        /// </summary>
+        [TestMethod]
+        public void Convert_Expression1_ToRpn_ReturnsCorrectRpn()
+        {
+            string input = "2+2*2";
+            string expected = "2 2 2 * + ";
+
+            string actual = RPNConverter.Convert(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Тест конвертации строки "(2+2)*2" в "2 2 + 2 *"
+        /// </summary>
+        [TestMethod]
+        public void Convert_Expression2_ToRpn_ReturnsCorrectRpn()
+        {
+            string input = "(2+2)*2";
+            string expected = "2 2 + 2 * ";
+
+            string actual = RPNConverter.Convert(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Тест конвертации строки "(-4)*(-2)" в "-4 -2 *"
+        /// </summary>
+        [TestMethod]
+        public void Convert_Expression3_ToRpn_ReturnsCorrectRpn()
+        {
+            string input = "(-4)*(-2)";
+            string expected = "-4 -2 * ";
+
+            string actual = RPNConverter.Convert(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Тест конвертации строки "65*5*51*25*55-(-35)*5-(-3)"
+        /// </summary>
+        [TestMethod]
+        public void Convert_Expression4_ToRpn_ReturnsCorrectRpn()
+        {
+            string input = "65*5*51*25*55-(-35)*5-(-3)";
+            string expected = "65 5 * 51 * 25 * 55 * -35 5 * - -3 - ";
+
+            string actual = RPNConverter.Convert(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
