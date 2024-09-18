c50fa953cbba57607efd0a8ac5d33d40d09fa44d
Ivan Zhenin <ofdof44488kkofsdkofs@mail.ru>
2024-09-18 21:31:30 +0500

    Add frame main ViewModel

---
+++ SimpleCalculator/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SimpleCalculator.Helpers;
+using SimpleCalculator.Model;
+using System.ComponentModel;
+using System.Windows.Input;
+
+
+namespace SimpleCalculator.ViewModel
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
