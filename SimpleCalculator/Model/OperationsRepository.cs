9215870421bdfd26d5ce908b30dd8d3df9067d31
Ivan Zhenin <ofdof44488kkofsdkofs@mail.ru>
2024-10-03 20:14:12 +0500

    Edit OperationRepository lists-logic

--- SimpleCalculator/Model/OperationsRepository.cs
+++ SimpleCalculator/Model/OperationsRepository.cs
@@ -1,25 +1,16 @@
 ﻿using SimpleCalculator.Model.Interfaces;
+using System.Reflection;
 
 namespace SimpleCalculator.Model
 {
     public static class OperationsRepository
     {
-        public static readonly IReadOnlyList<IOperator> OperatorList = new List<IOperator>()
-        {
-            new BracketOpen(),
-            new BracketClose(),
-            new Addition(),
-            new Subtraction(),
-            new Multiplication(),
-            new Division(),
-        }.AsReadOnly();
+        public static readonly IReadOnlyList<IOperator> OperatorList = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => typeof(IOperator).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+            .Select(Activator.CreateInstance).Cast<IOperator>().ToList();
 
-        public static readonly IReadOnlyList<IOperation> OperationList = new List<IOperation>()
-        {
-            new Addition(),
-            new Subtraction(),
-            new Multiplication(),
-            new Division(),
-        }.AsReadOnly();
+        public static readonly IReadOnlyList<IOperation> OperationList = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => typeof(IOperation).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+            .Select(Activator.CreateInstance).Cast<IOperation>().ToList();
     }
 }
